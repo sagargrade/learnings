9e790ba4324184f0aa257c23efd27ea5aa66cadb
sgd8w23 <sagar.grade@colruytgroup.com>
2024-07-31 05:09:17 +0200

    Java Script - Primitive

--- language/javascript/01-fundamentals/script.js
+++ language/javascript/01-fundamentals/script.js
@@ -1,3 +1,34 @@
+//---------------------------------------------------------
 // Variables
-let personName = "Ram";
-let personAge = 23;
+//---------------------------------------------------------
+let personName = "Ram"; // Variable (Mutable)
+let personAge = 23; // Variable (Mutable)
+const TOTAL_COUNT = 100; // Constant (Immutable)
+var avoidVarUsage = "Test" // Variable (Mutable) but global scope
+
+//---------------------------------------------------------
+// Data Types
+//---------------------------------------------------------
+/* Primitives - 7 
+    number (decimal, integer), 
+    string (sequence of characters), 
+    boolean, 
+    undefined (empty value), 
+    null (empty value), 
+    Symbol, 
+    BigInt (Large Integer)
+*/
+let numberDataType = 10;
+console.log(typeof numberDataType); // number
+
+let stringDateType = 'Test';
+console.log(typeof stringDateType); // string
+
+let booleanDataType = true;
+console.log(typeof booleanDataType); // boolean
+
+let undefinedDataType;
+console.log(typeof undefinedDataType); // undefined
+
+let nullDataType = null;
+console.log(typeof nullDataType); // object //bug in JS not corrected due to legacy reason
